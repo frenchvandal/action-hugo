601c0d70651dccd270ec9a51dc1d3cdaf2bd21cc
frenchavandal <42499534+frenchvandal@users.noreply.github.com>
2021-03-05 23:01:50 +0100

    test cache

--- src/main.ts
+++ src/main.ts
@@ -8,14 +8,15 @@ import { restoreCache, saveCache } from '@actions/cache';
     const paths = ['/opt/hostedtoolcache/hugo/0.81.0/x64/'];
     const key = 'hugo-v0.81.0';
     const cacheKey = await restoreCache(paths, key);
-    info(`cacheKey: ${cacheKey}`);
 
-    //await exec(`${await hugoExec()} ${getInput('args')}`);
+    if (cacheKey) {
+      await exec(`/opt/hostedtoolcache/hugo/0.81.0/x64/hugo ${getInput('args')}`);
+    } else {
+      await exec(`${await hugoExec()} ${getInput('args')}`);
 
-    await exec('/opt/hostedtoolcache/hugo/0.81.0/x64/hugo');
-
-    const cacheId = await saveCache(paths, key);
-    info(`cacheId: ${cacheId}`);
+      const cacheId = await saveCache(paths, key);
+      info(`cacheId: ${cacheId}`);
+    }
   } catch (e) {
     setFailed(`Action failed with error: ${e.message}`);
   }
