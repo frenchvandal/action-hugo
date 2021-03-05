a3853c70206418e2b3e47f7813acd5fab6085744
frenchavandal <42499534+frenchvandal@users.noreply.github.com>
2021-03-05 22:31:07 +0100

    recache

--- src/main.ts
+++ src/main.ts
@@ -6,15 +6,16 @@ import { restoreCache, saveCache } from '@actions/cache';
 (async (): Promise<void> => {
   try {
     const paths = ['/opt/hostedtoolcache/hugo/0.81.0/x64/'];
-    const key = '/hugo/0.81.0/x64/';
+    const key = 'hugo-v0.81.0';
     const cacheKey = await restoreCache(paths, key);
-    info(`cacheId: ${cacheKey}`);
+    info(`cacheKey: ${cacheKey}`);
 
-    //await exec(`${await hugoExec()} ${getInput('args')}`);
+    await exec(`${await hugoExec()} ${getInput('args')}`);
 
-    await exec('/opt/hostedtoolcache/hugo/0.81.0/x64/hugo');
+    //await exec('/opt/hostedtoolcache/hugo/0.81.0/x64/hugo');
 
-    //const cacheId = await saveCache(paths, key);
+    const cacheId = await saveCache(paths, key);
+    info(`cacheId: ${cacheId}`);
   } catch (e) {
     setFailed(`Action failed with error: ${e.message}`);
   }
